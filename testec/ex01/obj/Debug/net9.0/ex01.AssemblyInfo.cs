dcfe6cb269dd870aae37c0bda43a32196b9c1e74
JOHN PETER <joaopedroteixeira26@gmail.com>
2025-02-15 17:54:27 -0300

    Primeira parte dos estudos em c#

--- testec/ex01/obj/Debug/net9.0/ex01.AssemblyInfo.cs
+++ testec/ex01/obj/Debug/net9.0/ex01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbe6fc647995b6016f07b5610b2d62f24dfc0a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce6ae2edf251bfb006295d2116b6dd3f1d418ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
